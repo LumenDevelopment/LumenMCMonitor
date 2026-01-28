5ba18c5eaa3178576e0552f11f1848dacc9d349d
snvksvnk <98760003+snvksnvk@users.noreply.github.com>
2026-01-28 17:19:56 +0100

    Prep for Velocity

---
+++ velocity/src/main/java/Monitor.java
@@ -0,0 +1,10 @@
+import com.velocitypowered.api.plugin.Plugin;
+
+@Plugin(
+        id = "lumenmc-monitor",
+        name = "LumenMC Monitor",
+        version = "b1.1",
+        authors = {"Lumen Development", "SNVK"}
+)
+public class Monitor {
+}
